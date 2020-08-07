fad440e3a6d69c42f9df462be67cfeadf4de4cad
Ian Becker <ian-becker@lambdastudents.com>
2020-08-07 08:15:47 -0700

    First commit

---
+++ MovieSearch/MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Ian Becker on 8/7/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
